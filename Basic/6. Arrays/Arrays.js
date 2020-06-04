c630c588c75fca66d2e6c10853571b3d09ff0c01
sayanta28 <sayanta28@gmail.com>
2020-06-04 21:31:49 +0600

    Topic Basic Array

---
+++ Basic/6. Arrays/Arrays.js
@@ -0,0 +1,13 @@
+//Array is a data-structures which rerprersents Lists of Data or Items
+let selectedColours = ['red', 'blue']; // Declaration
+selectedColours[2] = 'green';
+selectedColours[3] = 3;
+//Its length is dynamic that means we can add elements in the run time.
+//Its type is also dynamic. Every Item or Object in Array can have diffrent data types. That's why JS is dynamic language.
+console.log(selectedColours);
+//First Index of Array is 0 and last is n-1.
+console.log(selectedColours[0]);
+console.log(typeof selectedColours); //Actually Array is an objects
+
+console.log(selectedColours.length); //returns th length of Array
+//More details wiil learn in array section.
