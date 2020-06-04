a50097a3db4420762d543413b23276d8b7a3a8ba
sayanta28 <sayanta28@gmail.com>
2020-06-04 11:17:19 +0600

    Rules of Var and Best prac of declaring JS Var

--- Basic/Variable/variable.js
+++ Basic/Variable/variable.js
@@ -1,4 +1,18 @@
-let name = 'Sayanta'
-console.log(name)
-let firstName='Anik'
-console.log(firstName)
+let data; // It is undefined
+let name = 'Sayanta';
+console.log(name);
+
+//Rules for Variable:
+// 1. Cannot be a reserved keyword
+// 2. Should be meaningful
+// 3. Cannot start with a number (1name)
+// 4. Cannot contain a space or hyphen(-)
+
+let firstName = 'Sayanta'; //Here we use Camel Notation. It is a naming convension
+let lastname = 'Chowdhury'; //Best practise is initialize one variable in one line
+console.log(firstName+' '+lastname);
+
+let anjanSon, ashokSon; //Variable Can be declare this way
+
+let anikVersity = 'AIUB', sayanVersity = 'BAIUST'; //This is not best practise. Declaration should be in different line
+console.log(anikVersity,sayanVersity);
