558bc2a1a972d3dd150cd56b1013ee1d5f3df162
sayanta28 <sayanta28@gmail.com>
2020-06-04 21:48:07 +0600

    JS: Basic Function

---
+++ Basic/7. Functions/Functions.js
@@ -0,0 +1,16 @@
+//Function is like a machine which can do a specific task.
+function greet(){
+    console.log('Hello Bro');
+}
+//Declaration of function without parameter.
+greet();
+
+function show(name, age){ // name is the parameter of the function
+    console.log('Hello ' + name +'. Age:'+age);
+}
+//Declaration of function with parameter.
+show('Sayanta',22); //Sayanta is a argument of the function
+// Argument is actuully the supply of the function parameter.
+show('Anik',32);
+
+
