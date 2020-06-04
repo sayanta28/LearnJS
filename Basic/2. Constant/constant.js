656494e9647b8ab8a0a1a664c9305362c0ba9055
sayanta28 <sayanta28@gmail.com>
2020-06-04 11:32:26 +0600

    New Branch for Constant

---
+++ Basic/2. Constant/constant.js
@@ -0,0 +1,11 @@
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+// Here the variable intererstRate Can be Update. So the value can be change.
+
+const interestRateRep = 0.3;
+// interrestRateRep = 1; (Error)
+
+console.log(interestRate);
+//But here interestRateRep cannot be change. Const keyword gives the security.Once it is initialize it cannot be changed.
+//That's why we should use const instead of let for giving the vasriable security.
