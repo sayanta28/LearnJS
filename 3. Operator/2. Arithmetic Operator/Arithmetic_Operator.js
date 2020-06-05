8b43bad8805002086907cf4ced3fe3c2f0debca2
sayanta28 <sayanta28@gmail.com>
2020-06-05 22:45:24 +0600

    Arithmetic Operator

---
+++ 3. Operator/2. Arithmetic Operator/Arithmetic_Operator.js
@@ -0,0 +1,22 @@
+let x = 10;
+let y = 30;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y); // It represents exponentiation; x^y(X to the power of Y) 
+
+//Increment
+console.log(x++);
+console.log(x);
+console.log(x++);
+console.log(x);
+
+//Decrement
+console.log(x--);
+console.log(x);
+console.log(--x);
+console.log(x);
+
